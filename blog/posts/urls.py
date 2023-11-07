d2a1813d0e43a376a10d09a63193fbea7c88b3bb
Yuliya Bayur <juliy14497@gmail.com>
2023-11-07 20:58:51 +0200

    Add views with model Post

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('', views.posts, name='all_posts'),
-    path('post/<int:post_id>', views.get_post, name='post')
+    path("", views.posts, name="all_posts"),
+    path("post/<int:post_id>", views.get_post, name="post"),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from posts.models import Post
 
 
 def get_post(request, post_id):
-    data = {'data': 'data', 'post_id': post_id, 'title': 'title'}
-    return render(request, 'posts/post.html', data)
+    post = get_object_or_404(Post, id=post_id)
+    data = {"post": post}
+    return render(request, "posts/post.html", data)
 
 
 def posts(request):
-    data = {'data': 'data'}
-    return render(request, 'posts/all_post.html', data)
+    posts = Post.objects.all()
+    data = {"posts": posts}
+    return render(request, "posts/all_post.html", data)
