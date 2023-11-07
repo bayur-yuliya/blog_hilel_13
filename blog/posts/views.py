7686509b519db1c9f3e059de9e3d62d755011f38
Yuliya Bayur <juliy14497@gmail.com>
2023-11-07 21:12:36 +0200

    Add redis and django cache

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 from posts.models import Post
 
 
+@cache_page(60 * 15)
 def get_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     data = {"post": post}
     return render(request, "posts/post.html", data)
 
 
+@cache_page(60 * 15)
 def posts(request):
-    posts = Post.objects.all()
-    data = {"posts": posts}
+    all_posts = Post.objects.all()
+    data = {"posts": all_posts}
     return render(request, "posts/all_post.html", data)
