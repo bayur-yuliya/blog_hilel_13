973e4a903cc7f13f711b1e47d829b609cfb0b6a4
Yuliya Bayur <juliy14497@gmail.com>
2023-11-08 20:36:44 +0200

    Add update post view, template

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,5 +4,6 @@ from posts import views
 
 urlpatterns = [
     path("", views.posts, name="all_posts"),
-    path("post/<int:post_id>", views.get_post, name="post"),
+    path("post/<int:post_id>/", views.get_post, name="post"),
+    path("post/<int:post_id>/update/", views.update_post, name="update_post"),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.decorators.cache import cache_page
+import time
 
+from django.core.cache import cache
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+
+from posts.forms import PostForm
 from posts.models import Post
 
 
-@cache_page(60 * 15)
+def posts(request):
+    all_posts = Post.objects.all()
+
+    data = {"posts": all_posts}
+    return render(request, "posts/all_post.html", data)
+
+
 def get_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+    post = cache.get(f'post-{post_id}')
+    if not post:
+        time.sleep(5)
+        post = get_object_or_404(Post, id=post_id)
+        cache.set(f'post-{post_id}', post, 60 * 15)
     data = {"post": post}
     return render(request, "posts/post.html", data)
 
 
-@cache_page(60 * 15)
-def posts(request):
-    all_posts = Post.objects.all()
-    data = {"posts": all_posts}
-    return render(request, "posts/all_post.html", data)
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "GET":
+        form = PostForm(instance=post)
+        return render(request, "posts/update_post.html", {'form': form})
+
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+        form.save()
+    cache.delete(f'post-{post_id}')
+    return redirect(reverse('post', args=[post_id]))
